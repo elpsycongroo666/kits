fc50fca5b0ad48bd299c30d02413287c3dad0e96
翔 <297962650@qq.com>
2019-07-11 19:49:56 +0800

    封装的获取时间和id的工具类

---
+++ kits.js
@@ -0,0 +1,30 @@
+let kits = {};
+kits.formateDate = function () {
+    let date = new Date();
+    let y = date.getFullYear();
+    let M = date.getMonth() + 1;
+    let d = date.getDate();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    M < 10 ? M = '0' + M : M;
+    d < 10 ? d = '0' + d : d;
+    h < 10 ? h = '0' + h : h;
+    m < 10 ? m = '0' + m : m;
+    s < 10 ? s = '0' + s : s;
+    return y + '-' + M + '-' + d + ' ' + h + ':' + m + ':' + s;
+}
+// 封装一个获得[n,m]之间的随机整数
+kits.ranDomInt = function(n,m){
+    return Math.floor(Math.random() * (m - n +1) + n);
+}
+/*
+    封装一个生成唯一id的方法
+*/
+kits.primaryKey = function(){
+    let now = Date.now();//获取当前毫秒数
+    // 为了防止在1毫秒之内生成的id有多个，再次加上一个大范围的随机数
+    let r = kits.ranDomInt(100000,999999);
+    // 把得到的两个结果拼接起来
+    return now + '' + r;
+}
